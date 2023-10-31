570cf13f9d2d846988cfd86804484ce150bf0f61
JohnsonMac <b081148@gmail.com>
2023-10-31 00:45:17 -0400

    update ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,29 +12,14 @@ module.exports = {
     "gatsby-transformer-sharp",
     `gatsby-plugin-theme-ui`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-1ZRX8FXSM",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
+        // your google analytics tracking id
+        trackingId: `G-1ZRX8FXSM8`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
         anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-
-        // defaults to false
-        enableWebVitalsTracking: true,
       },
     },
   ],
